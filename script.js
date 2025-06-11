88a5c1e688e31e3031ff128e634e224796c4204f
ub-victor <victoireushindi371@gmail.com>
2025-06-11 13:52:08 +0200

    Initial commit

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
-document.addEventListener('DOMContentLoaded', ()=>{
-    const card = document.getElementById('flip-card');
+// Wait until the DOM is fully loaded
+document.addEventListener('DOMContentLoaded', () => {
+  // Select the card element
+  const card = document.getElementById('flip-card');
 
-    card.addEventListener('click', ()=>{
-        card.classList.toogle('flipped');
-    })
-})
+  // Add a click event to toggle the 'flipped' class
+  card.addEventListener('click', () => {
+    card.classList.toggle('flipped');
+  });
+});
