3c6212a0e6c6d62c2c02bc3d9824e204f6ee4c65
ub-victor <victoireushindi371@gmail.com>
2025-06-11 13:51:25 +0200

    Initial commit

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', ()=>{
-    const card = document.getElementById('flip-xard');
+    const card = document.getElementById('flip-card');
 
     card.addEventListener('click', ()=>{
         card.classList.toogle('flipped');
